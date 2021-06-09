135e27bf78aac3ca6334d7f7d5824042e26a053e
Natalia Doskach <natalia.a.doskach@gmail.com>
2021-06-09 12:59:11 +0300

    import / export added

--- app/src/main/java/com/wndenis/snipsnap/MenuActivity.kt
+++ app/src/main/java/com/wndenis/snipsnap/MenuActivity.kt
@@ -1,9 +1,14 @@
 package com.wndenis.snipsnap
 
+import android.Manifest
+import android.app.Activity
+import android.app.Activity.RESULT_OK
 import android.content.Context
 import android.content.Intent
+import android.content.pm.PackageManager
 import android.net.Uri
 import android.os.Bundle
+import android.provider.DocumentsContract
 import android.util.Log
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
@@ -59,13 +64,34 @@ import androidx.compose.ui.text.input.KeyboardCapitalization
 import androidx.compose.ui.text.style.TextOverflow
 import androidx.compose.ui.unit.dp
 import androidx.compose.ui.unit.sp
+import androidx.core.app.ActivityCompat
+import androidx.core.app.ActivityCompat.startActivityForResult
 import com.vanpra.composematerialdialogs.MaterialDialog
 import com.vanpra.composematerialdialogs.buttons
+import com.wndenis.snipsnap.ui.theme.SnipsnapTheme
 import com.wndenis.snipsnap.ui.theme.Y400
 import java.io.File
 import java.time.Instant
 import java.time.LocalDateTime
 import java.time.ZoneId
+import android.os.ParcelFileDescriptor
+
+import android.os.AsyncTask
+import androidx.compose.material.icons.filled.Publish
+import androidx.core.content.ContextCompat
+import com.wndenis.snipsnap.MainActivity.Companion.getContext
+import com.wndenis.snipsnap.data.CalendarAdapter
+import com.wndenis.snipsnap.data.CalendarAdapter.Companion.importFromFile
+import java.io.BufferedReader
+import java.io.FileNotFoundException
+import java.io.FileOutputStream
+import java.io.IOException
+import java.io.InputStreamReader
+import java.lang.Exception
+
+
+private const val FILE_EXPORT_REQUEST_CODE = 12
+private const val PICK_FILE = 2
 
 class DiagramFile(
     var fileName: String = "",
@@ -75,27 +101,37 @@ class DiagramFile(
 )
 
 var FABClick: (() -> Unit)? = null
+var pathToFile: String = "";
 
 class MenuActivity : ComponentActivity() {
     @ExperimentalMaterialApi
     @ExperimentalComposeUiApi
     @OptIn(ExperimentalAnimationApi::class)
     public override fun onCreate(savedInstanceState: Bundle?) {
+        ActivityCompat.requestPermissions(
+            this,
+            arrayOf(
+                Manifest.permission.READ_EXTERNAL_STORAGE,
+                Manifest.permission.WRITE_EXTERNAL_STORAGE
+            ), 1
+        );
         super.onCreate(savedInstanceState)
         setTheme(R.style.SplashScreenTheme)
 
         setContent {
-            Scaffold(
-                topBar = { TopBarMain() },
-                floatingActionButton = { AddButton(this) }
-            ) {
-                val diagrams = remember { mutableStateListOf<DiagramFile>() }
-                val updater = {
-                    diagrams.clear()
-                    updateFileList(diagrams)
+            SnipsnapTheme {
+                Scaffold(
+                    topBar = { TopBarMain(this) },
+                    floatingActionButton = { AddButton(this) }
+                ) {
+                    val diagrams = remember { mutableStateListOf<DiagramFile>() }
+                    val updater = {
+                        diagrams.clear()
+                        updateFileList(diagrams)
+                    }
+                    updater()
+                    DiagramList(diagrams, updater, this)
                 }
-                updater()
-                DiagramList(diagrams, updater, this)
             }
         }
     }
@@ -121,6 +157,66 @@ class MenuActivity : ComponentActivity() {
                 )
             }
     }
+
+    override fun onActivityResult(requestCode: Int, resultCode: Int, data: Intent?) {
+        super.onActivityResult(requestCode, resultCode, data)
+        if (resultCode != RESULT_OK) return
+        when (requestCode) {
+            FILE_EXPORT_REQUEST_CODE -> if (data != null) {
+                val uri = data.data
+                if (uri != null) {
+                    val contentResolver = contentResolver
+                    try {
+                        val file = File(pathToFile)
+                        val jsonRepr = file.readText()
+                        contentResolver.openFileDescriptor(uri, "w")?.use {
+                            FileOutputStream(it.fileDescriptor).use {
+                                it.write(
+                                    jsonRepr.toByteArray()
+                                )
+                            }
+                        }
+                    } catch (e: FileNotFoundException) {
+                        e.printStackTrace()
+                    } catch (e: IOException) {
+                        e.printStackTrace()
+                    }
+                }
+            }
+            PICK_FILE -> if (data != null) {
+                val uri = data.data
+                if (uri != null) {
+                    val contentResolver = contentResolver
+                    try {
+                        val stringBuilder = StringBuilder()
+                        contentResolver.openInputStream(uri)?.use { inputStream ->
+                            BufferedReader(InputStreamReader(inputStream)).use { reader ->
+                                var line: String? = reader.readLine()
+                                while (line != null) {
+                                    stringBuilder.append(line)
+                                    line = reader.readLine()
+                                }
+                            }
+                        }
+                        Log.e("obj", stringBuilder.toString())
+                        //add new file
+                        val folder = this.filesDir
+                        val filename = "importedFile.spsp"
+                        val file = File(folder, filename)
+                        if (file.exists()) {
+                            file.delete()
+                        }
+                        val res = file.writeText(stringBuilder.toString())
+                        startEditing(filename, false, this)
+                    } catch (e: FileNotFoundException) {
+                        e.printStackTrace()
+                    } catch (e: IOException) {
+                        e.printStackTrace()
+                    }
+                }
+            }
+        }
+    }
 }
 
 fun startEditing(name: String, isNew: Boolean, context: Context) {
@@ -131,12 +227,22 @@ fun startEditing(name: String, isNew: Boolean, context: Context) {
 }
 
 @Composable
-fun TopBarMain() {
+fun TopBarMain(context: Context) {
     TopAppBar(
         title = { Text("Мои диаграммы") },
         navigationIcon = {
         },
-        actions = {}
+        actions = {
+            IconButton(
+                onClick = {
+                    val intent2 = Intent(Intent.ACTION_OPEN_DOCUMENT).apply {
+                        addCategory(Intent.CATEGORY_OPENABLE)
+                        type = "application/spsp"
+                    }
+                    startActivityForResult(context as Activity, intent2, PICK_FILE, null)
+                }
+            ) { Icon(imageVector = Icons.Filled.Publish, contentDescription = "export") }
+        }
     )
 }
 
@@ -163,7 +269,7 @@ fun DiagramCard(
         shape = RoundedCornerShape(18.dp),
         elevation = 4.dp,
 
-    ) {
+        ) {
         Spacer(modifier = Modifier.height(16.dp))
         Row(
             modifier = Modifier
@@ -206,26 +312,26 @@ fun DiagramCard(
                 Row {
                     IconButton(
                         onClick = {
-
-                            val sendIntent = Intent().apply {
-                                action = Intent.ACTION_SEND
-                                putExtra(
-                                    Intent.EXTRA_STREAM,
-                                    Uri.parse("file://" + diagram.fullPath)
-                                )
-                                putExtra(Intent.EXTRA_SUBJECT, "Поделиться диаграммой")
-                                putExtra(Intent.EXTRA_TEXT, "Ура, прилетела диаграмма")
-                                type = "application/json"
+                            pathToFile = diagram.fullPath;
+                            val intent = Intent(Intent.ACTION_CREATE_DOCUMENT).apply {
+                                addCategory(Intent.CATEGORY_OPENABLE)
+                                type = "application/spsp"
+                                putExtra(Intent.EXTRA_TITLE, diagram.fileName)
+                                //   putExtra(DocumentsContract.EXTRA_INITIAL_URI, pickerInitialUri)
                             }
-                            val shareIntent =
-                                Intent.createChooser(sendIntent, "Поделиться диаграммой")
-                            // context.startActivity(shareIntent)
+                            startActivityForResult(
+                                context as Activity,
+                                intent,
+                                FILE_EXPORT_REQUEST_CODE,
+                                null
+                            )
+
                         }
                     ) {
                         Icon(
                             imageVector = Icons.Filled.Share,
                             contentDescription = "export",
-                            tint = Color.White
+                            //                          tint = Color.White,
                         )
                     }
                     IconButton(
@@ -345,6 +451,7 @@ fun DiagramList(diagrams: MutableList<DiagramFile>, updater: () -> Unit, context
         }
     }
 }
+
 
 @Composable
 fun AddButton(context: Context) {
